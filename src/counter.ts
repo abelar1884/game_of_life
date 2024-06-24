a90e480594235f548a0e6da05aa31c5b1139be76
abelar1884 <hamster1884@gmail.com>
2024-06-24 10:21:52 +0300

    init

---
+++ src/counter.ts
@@ -0,0 +1,13 @@
+function a() {
+  console.log('a');
+}
+
+export function setupCounter(element: HTMLButtonElement) {
+  let counter = 0;
+  const setCounter = (count: number) => {
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener('click', () => setCounter(counter + 1));
+  setCounter(0);
+}
